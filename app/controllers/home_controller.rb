f1e87e67970a5624496f8fb86e5f00690e8b0f89
Ee Shan <eeshansim@gmail.com>
2017-06-23 12:59:39 +0800

    List posts in home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
   def index
+     @posts = Post.includes(:user, :location).order("created_at DESC").paginate(page: params[:page])
   end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,7 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.json
   def index
-    @posts = Post.all
+    @posts = Post.includes(:user, :location).all.paginate(page: params[:page])
   end
 
   # GET /posts/1
@@ -69,6 +69,6 @@ class PostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.require(:post).permit(:body)
+      params.require(:post).permit(:body, :page)
     end
 end
