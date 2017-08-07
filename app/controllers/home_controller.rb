2e11de385e02df00e9b9bf25c0a4150219f610a3
Ee Shan <eeshansim@gmail.com>
2017-08-07 10:08:20 +0800

    fix syntax

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   def index
      @posts = Post.includes(:user, :location).order("created_at DESC").paginate(page: params[:page])
 
-     track_day = (params[:date] || Time.now.in_time_zone("Singapore"))&.to_date
+     track_day = (params[:date] || Time.now.in_time_zone("Singapore")).to_date
      @current_date = track_day
      gon.watch.tracks = Track.includes(:location).
                               where("DATE(created_at AT TIME ZONE 'UTC' AT TIME ZONE ?) = ?",
